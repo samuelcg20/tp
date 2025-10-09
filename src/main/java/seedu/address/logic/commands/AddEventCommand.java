51d902f52251f94c84debe74c85ea64826453118
saajid-ahamed <e1398310@u.nus.edu>
2025-10-09 21:41:42 +0800

    Fix bugs

--- src/main/java/seedu/address/logic/commands/AddEventCommand.java
+++ src/main/java/seedu/address/logic/commands/AddEventCommand.java
@@ -5,7 +5,7 @@ import static java.util.Objects.requireNonNull;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.event.Event;
+
 
 /**
  * Adds an event to the event book.
@@ -21,30 +21,17 @@ public class AddEventCommand extends Command {
     public static final String MESSAGE_SUCCESS = "New event added: %1$s";
     public static final String MESSAGE_DUPLICATE_EVENT = "This event already exists in the event book";
 
-    private final Event toAdd;
+
 
     /**
      * Creates an AddEventCommand to add the specified {@code Event}.
      */
-    public AddEventCommand(Event event) {
-        requireNonNull(event);
-        this.toAdd = event;
+    public AddEventCommand() {
+
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        requireNonNull(model);
-        if (model.hasEvent(toAdd)) {
-            throw new CommandException(MESSAGE_DUPLICATE_EVENT);
-        }
-        model.addEvent(toAdd);
-        return CommandResult.forEvents(String.format(MESSAGE_SUCCESS, Messages.format(toAdd)));
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        return other == this
-                || (other instanceof AddEventCommand
-                && toAdd.equals(((AddEventCommand) other).toAdd));
+       return null;
     }
 }
--- src/main/java/seedu/address/logic/commands/DeleteEventCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteEventCommand.java
@@ -1,7 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_INDEX;
+
 
 import java.util.List;
 
@@ -9,7 +9,7 @@ import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.event.Event;
+
 
 /**
  * Deletes an event identified using it's displayed index from the event list.
@@ -20,7 +20,7 @@ public class DeleteEventCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Deletes the event identified by the index number used in the displayed event list.\n"
-            + "Parameters: " + PREFIX_INDEX + "INDEX (must be a positive integer)\n"
+            + "Parameters: " + "INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
     public static final String MESSAGE_DELETE_EVENT_SUCCESS = "Deleted Event: %1$s";
@@ -30,25 +30,7 @@ public class DeleteEventCommand extends Command {
     public DeleteEventCommand(Index targetIndex) {
         this.targetIndex = targetIndex;
     }
-
-    @Override
     public CommandResult execute(Model model) throws CommandException {
-        requireNonNull(model);
-        List<Event> lastShownList = model.getFilteredEventList();
-
-        if (targetIndex.getZeroBased() >= lastShownList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_EVENT_DISPLAYED_INDEX);
-        }
-
-        Event eventToDelete = lastShownList.get(targetIndex.getZeroBased());
-        model.deleteEvent(eventToDelete);
-        return CommandResult.forEvents(String.format(MESSAGE_DELETE_EVENT_SUCCESS, Messages.format(eventToDelete)));
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof DeleteEventCommand // instanceof handles nulls
-                && targetIndex.equals(((DeleteEventCommand) other).targetIndex)); // state check
+        return null;
     }
 }
