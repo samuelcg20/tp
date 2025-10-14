6ddb9a934440f4e12d35b1c53adbcbe73d23ea69
KaungKhantMinn <khantminn171@gmail.com>
2025-10-14 23:10:23 +0800

    Add ListCommandParser class

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -12,4 +12,7 @@ public abstract class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Lists the members/events.\n"
+            + "Example: " + COMMAND_WORD + " member/event";
 }
