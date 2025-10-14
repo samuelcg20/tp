538958df3c31f221fe0fc57d6af51839ded534f9
Prateek2030s <194741861+Prateek2030s@users.noreply.github.com>
2025-10-14 22:05:57 +0800

    @Update import order

--- src/main/java/seedu/address/logic/commands/event/DeleteEventCommand.java
+++ src/main/java/seedu/address/logic/commands/event/DeleteEventCommand.java
@@ -1,9 +1,9 @@
 package seedu.address.logic.commands.event;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.logic.commands.CommandResult;
 import seedu.address.model.Model;
 
 /**
