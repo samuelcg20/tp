2a530fa651f900354abf1eef38454c50589e8657
saajid-ahamed <e1398310@u.nus.edu>
2025-10-15 22:54:48 +0800

    Add role tag to member

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -10,7 +10,7 @@ public abstract class AddCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a member or event to the address book. "
             + "Parameters: TYPE (either 'member' or 'event') "
             + "[n/NAME] [p/PHONE] [e/EMAIL] [a/ADDRESS] "
-            + "[t/TAG]... "
+            + "[r/ROLE]... "
             + "[n/EVENTNAME] [d/DATE] [l/VENUE] "
             + "Example: " + COMMAND_WORD
             + " member n/John Doe p/98765432";
