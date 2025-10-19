68bd176b2310a2b22ac07cada70b604c70922779
KaungKhantMinn <khantminn171@gmail.com>
2025-10-19 12:34:59 +0800

    Fix bug in ListCommandParser class

--- src/main/java/seedu/address/logic/parser/ListCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListCommandParser.java
@@ -17,7 +17,7 @@ public class ListCommandParser implements Parser<ListCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the ListCommand
      * and returns a ListMemberCommand/ ListEventCommand object for execution.
-     * @throws ParseException if the user input does not conform the expected format
+     * @throws ParseException if the user input does not conform to the expected format
      */
     public ListCommand parse(String args) throws ParseException {
         // Split the current args input into at most 2 parts: [member/event, unnecessary information]
@@ -25,7 +25,7 @@ public class ListCommandParser implements Parser<ListCommand> {
 
         if (argsParts.length != 1) {
             throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteMemberCommand.MESSAGE_USAGE));
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_USAGE));
         }
 
         // Type indicates whether to list members or events
