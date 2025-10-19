9c5ea977500b47cc31db7fbb093ac33477bad2ac
Prateek2030s <194741861+Prateek2030s@users.noreply.github.com>
2025-10-19 19:52:52 +0800

    Create AliasBook class

---
+++ src/main/java/seedu/address/model/alias/AliasBook.java
@@ -0,0 +1,30 @@
+package seedu.address.model.alias;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AliasBook {
+
+    private final Map<String, String> aliasMap = new HashMap<>();
+
+    public void addAlias(Alias alias) {
+        aliasMap.put(alias.getAliasWord(), alias.getCommandWord());
+    }
+
+    public boolean isAliasPresent(String aliasWord) {
+        return aliasMap.containsKey(aliasWord);
+    }
+
+    public String getCommandWordForAlias(String aliasWord) {
+        return aliasMap.get(aliasWord);
+    }
+
+    public void clear() { // Method for future use
+        aliasMap.clear();
+    }
+
+    @Override
+    public String toString() {
+        return aliasMap.toString();
+    }
+}
