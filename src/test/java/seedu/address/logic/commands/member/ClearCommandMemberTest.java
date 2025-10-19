3b3e2c7b7b159b17bb24a042553c787013b719c7
KaungKhantMinn <khantminn171@gmail.com>
2025-10-19 13:18:58 +0800

    Add clear command test classes

--- src/test/java/seedu/address/logic/commands/member/ClearCommandMemberTest.java
+++ src/test/java/seedu/address/logic/commands/member/ClearCommandMemberTest.java
@@ -1,18 +1,18 @@
-package seedu.address.logic.commands;
+package seedu.address.logic.commands.member;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.member.ClearMemberCommand;
-import seedu.address.model.AddressBook;
+import seedu.address.logic.commands.CommandResult;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 
-public class ClearCommandTest {
-
+public class ClearCommandMemberTest {
     @Test
     public void execute_emptyAddressBook_success() {
         Model model = new ModelManager();
@@ -22,12 +22,13 @@ public class ClearCommandTest {
     }
 
     @Test
-    public void execute_nonEmptyAddressBook_success() {
+    public void execute_nonNullModel_success() {
         Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-        Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-        expectedModel.setAddressBook(new AddressBook());
 
-        assertCommandSuccess(new ClearMemberCommand(), model, ClearMemberCommand.MESSAGE_SUCCESS, expectedModel);
+        ClearMemberCommand command = new ClearMemberCommand();
+        CommandResult result = command.execute(model);
+
+        assertNotNull(result);
+        assertEquals(ClearMemberCommand.MESSAGE_SUCCESS, result.getFeedbackToUser());
     }
-
 }
