4830e71c28e9bb864f3fbd215a5232f5bcdbd365
saajid-ahamed <e1398310@u.nus.edu>
2025-10-21 13:19:41 +0800

    Add edit event feature

---
+++ src/main/java/seedu/address/logic/commands/event/EditEventCommand.java
@@ -0,0 +1,189 @@
+package seedu.address.logic.commands.event;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_EVENTS;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.CollectionUtil;
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.event.Date;
+import seedu.address.model.event.Event;
+import seedu.address.model.event.EventName;
+import seedu.address.model.event.Venue;
+
+/**
+ * Edits the details of an existing person in the address book.
+ */
+public class EditEventCommand extends EditCommand {
+
+    public static final String MESSAGE_EDIT_EVENT_SUCCESS = "Edited Event: %1$s";
+    public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
+    public static final String MESSAGE_DUPLICATE_EVENT = "This event already exists in the address book.";
+
+    private final Index index;
+    private final EditEventDescriptor editEventDescriptor;
+
+    // ======================================= EditMemberDescriptor class =======================================
+    /**
+     * A descriptor class that stores details to edit a {@code Person}.
+     * Each non-null field value replaces the corresponding field value of the target {@code Person}.
+     */
+    public static class EditEventDescriptor extends EditCommand.EditDescriptor {
+        private EventName eventName;
+        private Date date;
+        private Venue venue;
+
+
+        public EditEventDescriptor() {}
+
+        /**
+         * Copy constructor.
+         * A defensive copy of {@code tags} is used internally.
+         */
+        public EditEventDescriptor(EditEventDescriptor toCopy) {
+            setEventName(toCopy.eventName);
+            setDate(toCopy.date);
+            setVenue(toCopy.venue);
+        }
+
+        /**
+         * Returns true if at least one field is edited.
+         */
+        @Override
+        public boolean isAnyFieldEdited() {
+            return CollectionUtil.isAnyNonNull(eventName, date, venue);
+        }
+
+        public void setEventName(EventName eventName) {
+            this.eventName = eventName;
+        }
+
+        public Optional<EventName> getEventName() {
+            return Optional.ofNullable(eventName);
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public Optional<Date> getDate() {
+            return Optional.ofNullable(date);
+        }
+
+        public void setVenue(Venue venue) {
+            this.venue = venue;
+        }
+
+        public Optional<Venue> getVenue() {
+            return Optional.ofNullable(venue);
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if (other == this) {
+                return true;
+            }
+
+            // instanceof handles nulls
+            if (!(other instanceof EditEventDescriptor)) {
+                return false;
+            }
+
+            EditEventDescriptor otherEditEventDescriptor = (EditEventDescriptor) other;
+            return Objects.equals(eventName, otherEditEventDescriptor.eventName)
+                    && Objects.equals(date, otherEditEventDescriptor.date)
+                    && Objects.equals(venue, otherEditEventDescriptor.venue);
+        }
+
+        @Override
+        public String toString() {
+            return new ToStringBuilder(this)
+                    .add("event name", eventName)
+                    .add("date", date)
+                    .add("venue", venue)
+                    .toString();
+        }
+    }
+    // ==================================================================================================
+
+    /**
+     * @param index of the person in the filtered person list to edit
+     * @param editEventDescriptor details to edit the person with
+     */
+    public EditEventCommand(Index index, EditEventDescriptor editEventDescriptor) {
+        super();
+        requireNonNull(index);
+        requireNonNull(editEventDescriptor);
+
+        this.index = index;
+        this.editEventDescriptor = new EditEventDescriptor(editEventDescriptor);
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Event> lastShownList = model.getFilteredEventList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_EVENT_DISPLAYED_INDEX);
+        }
+
+        Event eventToEdit = lastShownList.get(index.getZeroBased());
+        Event editedEvent = createEditedEvent(eventToEdit, editEventDescriptor);
+
+        if (!eventToEdit.isSameEvent(editedEvent) && model.hasEvent(editedEvent)) {
+            throw new CommandException(MESSAGE_DUPLICATE_EVENT);
+        }
+
+        model.setEvent(eventToEdit, editedEvent);
+        model.updateFilteredEventList(PREDICATE_SHOW_ALL_EVENTS);
+        return new CommandResult(String.format(MESSAGE_EDIT_EVENT_SUCCESS, Messages.format(editedEvent)));
+    }
+
+    /**
+     * Creates and returns a {@code Person} with the details of {@code personToEdit}
+     * edited with {@code editPersonDescriptor}.
+     */
+    private static Event createEditedEvent(Event eventToEdit, EditEventDescriptor editEventDescriptor) {
+        assert eventToEdit != null;
+
+        EventName updatedEventName = editEventDescriptor.getEventName().orElse(eventToEdit.getName());
+        Date updatedDate = editEventDescriptor.getDate().orElse(eventToEdit.getDate());
+        Venue updatedVenue = editEventDescriptor.getVenue().orElse(eventToEdit.getVenue());
+
+        return new Event(updatedEventName, updatedDate, updatedVenue);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof EditCommand)) {
+            return false;
+        }
+
+        EditEventCommand otherEditCommand = (EditEventCommand) other;
+        return index.equals(otherEditCommand.index)
+                && editEventDescriptor.equals(otherEditCommand.editEventDescriptor);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("index", index)
+                .add("editMemberDescriptor", editEventDescriptor)
+                .toString();
+    }
+}
