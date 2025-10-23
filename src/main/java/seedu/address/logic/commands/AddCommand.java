63dc5f86357f12a3cd9ab016762ac7852d7be71f
sujith <s.bill.sujith.kumaar2@gmail.com>
2025-10-23 09:36:15 +0800

    AddCommand: Fix inconsistent terminologies

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -8,8 +8,8 @@ public abstract class AddCommand extends Command {
     public static final String COMMAND_WORD = "add";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a member or event to the address book.\n"
-            + "Parameters for member: add member n/[member name] p/[phone] e/[email] y/[year(1-4)] t/[roles...]\n"
-            + "Example: add member n/John Doe p/98765432 e/johndoe@gmail.com y/1 t/President\n"
+            + "Parameters for member: add member n/[member name] p/[phone] e/[email] y/[year(1-4)] r/[roles...]\n"
+            + "Example: add member n/John Doe p/98765432 e/johndoe@gmail.com y/1 r/President\n"
             + "Parameters for event: add event n/[event name] d/[date(YYYY-MM-DD)] l/[location]\n"
             + "Example: add event n/CS Workshop d/2025-12-30 l/NUS COM1-03\n";
 }
