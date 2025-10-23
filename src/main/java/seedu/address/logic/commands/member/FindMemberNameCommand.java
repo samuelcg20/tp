111374e633a8192f3b65995d16da3e6cbd1cc270
Prateek2030s <194741861+Prateek2030s@users.noreply.github.com>
2025-10-23 19:06:42 +0800

    Fix GUI switch

--- src/main/java/seedu/address/logic/commands/member/FindMemberNameCommand.java
+++ src/main/java/seedu/address/logic/commands/member/FindMemberNameCommand.java
@@ -30,7 +30,11 @@ public class FindMemberNameCommand extends FindCommand {
         requireNonNull(model);
         model.updateFilteredPersonList(predicate);
         return new CommandResult(
-                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()),
+                false,
+                false,
+                true,
+                false);
     }
 
     @Override
