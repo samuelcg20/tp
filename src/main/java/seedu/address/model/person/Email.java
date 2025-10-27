d174e43c75136a95754b9eae06d3daad72a74275
sujith <s.bill.sujith.kumaar2@gmail.com>
2025-10-28 07:25:27 +0800

    Update ug

--- src/main/java/seedu/address/model/person/Email.java
+++ src/main/java/seedu/address/model/person/Email.java
@@ -13,7 +13,7 @@ public class Email {
     public static final String MESSAGE_CONSTRAINTS = "Emails should be of the format local-part@u.nus.edu "
             + "and adhere to the following constraints:\n"
             + "1. The local-part should only contain alphanumeric characters and these special characters, excluding "
-            + "the parentheses, (" + SPECIAL_CHARACTERS + "). The local-part may not start or end with any special "
+            + "the parentheses, (" + SPECIAL_CHARACTERS + "). The local-part should also not start or end with any special "
             + "characters.\n"
             + "2. The domain must be exactly '@u.nus.edu'.";
     // alphanumeric and special characters
