9ceb9170c6e3401cbf2f7e2e8ccbd1d1060e417b
Prateek2030s <194741861+Prateek2030s@users.noreply.github.com>
2025-10-29 20:40:29 +0800

    Fix style issues

--- src/test/java/seedu/address/model/alias/AliasTest.java
+++ src/test/java/seedu/address/model/alias/AliasTest.java
@@ -1,8 +1,8 @@
 package seedu.address.model.alias;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
