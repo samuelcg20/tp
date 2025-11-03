7f291129d98168982fb3dd43401d485afc1cdcb0
samuelcg20 <samuelchristygeorge@u.nus.edu>
2025-11-03 16:55:43 +0800

    Remove unused imports and reorder imports

--- src/test/java/seedu/address/logic/parser/EditCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/EditCommandParserTest.java
@@ -16,8 +16,8 @@ import seedu.address.logic.commands.member.EditMemberCommand.EditMemberDescripto
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Phone;
-import seedu.address.model.tag.Tag;
 import seedu.address.model.person.Year;
+import seedu.address.model.tag.Tag;
 import seedu.address.testutil.EditMemberDescriptorBuilder;
 
 /**
